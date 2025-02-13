9dae827c7a2c987252cbb13b711d3f1283a198cd
Jacques de Villiers <jacquesdivi@gmail.com>
2025-02-13 16:38:45 +0200

    update the run.sh files with a new env var and termination

--- v_6.6/run.sh
+++ v_6.6/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ "$WP_CUSTOM_INI" ]; then
+if [ "$WORDPRESS_CUSTOM_INI" ]; then
     echo "Writing custom ini values"
-    printf "$WP_CUSTOM_INI" > $PHP_INI_DIR/conf.d/custom.ini
+    printf "$WORDPRESS_CUSTOM_INI" > $PHP_INI_DIR/conf.d/custom.ini
 fi
 
-service nginx start 
+set +e
+service nginx start
 docker-entrypoint.sh php-fpm
+service nginx stop
